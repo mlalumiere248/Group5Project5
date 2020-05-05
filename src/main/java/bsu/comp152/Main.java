d3f8ee3e6a76d4d51fc50c173c3a0d3648740927
johns <jsantore@bridgew.edu>
2020-05-05 17:22:39 -0400

    Added a comment to Main.java

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -8,7 +8,7 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-
+    //demo comment for video
     @Override
     public void start(Stage primaryStage) {
 
