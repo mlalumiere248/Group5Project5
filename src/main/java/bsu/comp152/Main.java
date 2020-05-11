0f9f3fc056ee46617fb116c6d2d134300cf95fb4
mlalumiere248 <mlalumiere@student.bridgew.edu>
2020-05-10 23:38:58 -0400

    Added some of the phone API components. Started adding things to main(opens basic window which we will need to make into a 'window'. Still trying to figure out how to fix the controllers and handlers.

--- src/main/java/bsu/comp152/Main.java
+++ src/main/java/bsu/comp152/Main.java
@@ -1,16 +1,61 @@
 package bsu.comp152;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
-    //demo comment for video
+
     @Override
     public void start(Stage primaryStage) {
-
+        Parent root = null;
+        var loc = getClass().getResource("Main.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e){
+            System.out.println("Couldn't Find FXML file!!!!!!");
+        }
+        Scene windowContents = new Scene(root, 300,400);
+        primaryStage.setScene(windowContents);
+        primaryStage.setTitle("Selecting an API");
+        primaryStage.show();
     }
-}
+
+    @FXML
+    public void exit(ActionEvent event){
+        System.exit(0);//alls well - bye bye
+    }
+
+    @FXML
+    public  void openPhoneWindow(ActionEvent event){
+        Parent root = null;
+        var loc = getClass().getResource("PhoneWindow.fxml");
+        try {
+            root = FXMLLoader.load(loc);
+        }catch (IOException e){
+            System.out.println("Couldn't Find FXML file!!!!!!");
+        }
+        //here is where we are creating the new window
+        Scene windowContents = new Scene(root, 900,400);
+        Stage phoneWindow = new Stage();
+        phoneWindow.setScene(windowContents);
+        phoneWindow.setTitle("Phone Code");
+        phoneWindow.show();
+    }
+    }
+
+
+
+
+
+
